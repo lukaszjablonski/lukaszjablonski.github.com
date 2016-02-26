2cf92a2d0cfcdf166545b351dc3a8da5f5c2b3e8
lukaszjablonski <lukasz.jablonski@wp.eu>
2016-02-26 15:22:32 +0100

    Update github.js

--- js/github.js
+++ js/github.js
@@ -1,6 +1,6 @@
 jQuery.githubUser = function(username, callback) {
   //jQuery.getJSON("http://github.com/api/v1/json/" + username + "?callback=?", callback);
-  jQuery.getJSON("https://api.github.com/users/" + username + "/repos?callback=?", callback);
+  jQuery.getJSON("https://api.github.com/users/" + username + "/repos", callback);
 }
  
 jQuery.fn.loadRepositories = function(username) {
